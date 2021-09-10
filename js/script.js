722e05d36db4ca74504486de235c4e1123848dd9
Brenna Spademan <bspademan87@gmail.com>
2021-09-10 11:37:04 -0400

    Added API to fetch repo data

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // Div where profile info will appear
 const profileElement = document.querySelector(".overview");
 const username = "b-spademan";
+repoList = document.querySelector(".repo-list");
 
 // Async function to get API data from github
 getUser = async function () {
     const resource = await fetch(`https://api.github.com/users/${username}`);
     const user = await resource.json();
-    console.log(user);
+    // console.log(user);
     displayUserInfo(user);
 };
 getUser();
@@ -26,4 +27,21 @@ const displayUserInfo = function (user) {
         <p><strong>Number of public repos:</strong> ${user.public_repos}</p>
     </div>`;
     profileElement.append(userInfo);
+    getRepos();
+};
+
+const getRepos = async function () {
+    const githubRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await githubRepos.json();
+    console.log(repoData);
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const details of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${details.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
